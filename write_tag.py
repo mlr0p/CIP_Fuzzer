b364c0ce4b3a34f8d398bf24111207a051511cf0
mlr0p <miuowwq@gmail.com>
2019-07-16 20:21:39 +1200

    added simple write tag

--- write_tag.py
+++ write_tag.py
@@ -4,31 +4,42 @@ import sys
 sys.path.append('../scapy-cip-enip')
 import time
 
-
 from cip import CIP, CIP_Path
 import plc
 import scapy
-
+import struct
 
 logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
 
-# Connect to PLC
-client = plc.PLCClient('192.168.9.227')
-if not client.connected:
-    sys.exit(1)
+def simple_write_tag(client, string):
+    string = sys.argv[1]
+    # Tag Type Service Parameter for Structures (\xa0\x02 + 4-byte structure handle) + Length (DINT - 4 bytes) + String (131 bytes, padded with null bytes)
+    tag_type_service_param = "\xa0\x02\xbc\x2c\x01\x00"
+    data = tag_type_service_param+ struct.pack('<I',len(string)) + string + '\x00' * (132 - len(string))
+    cippkt = CIP(service=0x4d, path=CIP_Path.make(class_id=0x6B, instance_id=0x227)) / data
+    client.send_unit_cip(cippkt)
+    resppkt = client.recv_enippkt()
+    print(resppkt[CIP].status)
 
-# Creating Connections Through the Connection Manager Object 
-if not client.forward_open():
-    sys.exit(1)
+def main():
+    # Connect to PLC
+    client = plc.PLCClient('192.168.9.227')
+    if not client.connected:
+        sys.exit(1)
 
-string = "modified_recipe"
-# Tag Type Service Parameter for Structures (\xa0\x02 + 4-byte structure handle) + Length (DINT - 4 bytes) + String (131 bytes, padded with null bytes)
-tag_type_service_param = "\xa0\x02\xbc\x2c\x01\x00"
-data = tag_type_service_param+ "\x0f\x00\x00\x00" + string + '\x00' * (132 - len(string))
-cippkt = CIP(service=0x4d, path=CIP_Path.make(class_id=0x6B, instance_id=0x227)) / data
-client.send_unit_cip(cippkt)
-resppkt = client.recv_enippkt()   
-resppkt.show()
+    # Creating Connections Through the Connection Manager Object 
+    if not client.forward_open():
+        sys.exit(1)
 
-# Close the connection
-client.forward_close()
+    # Fuzz the interface handle
+    # fuzz_interfacehandle(client)
+    # fuzz_timeout(client)
+    # fuzz_instanceid(client, 0xB2)
+    # fuzz_classid(client, 0x1f6)
+    # fuzz_pathsize(client, 0xB2, 0x1f6)
+    simple_write_tag(client, sys.argv[1])
+    # Close the connection
+    client.forward_close()
+
+if __name__ == '__main__':
+    main()
